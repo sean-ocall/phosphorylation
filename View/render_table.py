d3df6edc112854409d605073fe085bf15b433e34
Sean O'Callaghan <sean.ocall@gmail.com>
2017-10-24 16:32:14 +1100

    Added some rendering code, and code for translating uniprot to genename

---
+++ View/render_table.py
@@ -0,0 +1,41 @@
+import argparse, sqlite3, os
+from jinja2 import Environment, FileSystemLoader
+
+help_msg = "Grabs data from DB and renders to html"
+parser = argparse.ArgumentParser(description=help_msg)
+parser.add_argument('--databasefile', help='sqlite3 file (or blank)')
+
+def render_table(proteins_dict):
+    THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+    j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
+                         trim_blocks=True)
+    print j2_env.get_template('templates/test_template.html').render\
+        (proteins=proteins_dict)
+
+def get_proteins_from_db(db_cursor):
+    proteins_cursor = db_cursor.execute("SELECT * FROM proteintb;")
+    proteins = proteins_cursor.fetchall()
+
+    proteins_listofdicts = []
+    
+    for protein in proteins:
+        p_dict = {}
+        p_dict['proteinid'] = protein[0]
+        p_dict['uniprotid'] = protein[1]
+        p_dict['genenameid'] = protein[2]
+        p_dict['function'] = protein[3]
+
+        proteins_listofdicts.append(p_dict)
+
+    return proteins_listofdicts
+
+if __name__=="__main__":
+    args = parser.parse_args()
+    conn = sqlite.connect(args.databasefile)
+    db_cursor = conn.cursor()
+
+    proteins_listofdicts = get_proteins_from_db(db_cursor)
+    render_table(proteins_listofdicts)
+
+    conn.close()
+    
